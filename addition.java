9a444095c7197ba9347d60a3d0779bbd2c931f77
Kavinaya06 <51779981+Kavinaya06@users.noreply.github.com>
2023-09-11 16:03:38 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class addition {
+
+
+
+public static void main(String args[])
+{
+int a,b,c,d=10,e;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter two numbers:");
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+e=a+d;
+System.out.println("Addition: "+c);
+System.out.println("Addition of "+a+"and "+b+"is9:"+c);
+System.out.println("Addition: "+e);
+
+
+
+}
+}
