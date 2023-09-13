af49b7b7cff303b896cb42767c525204ecfff475
Kavinaya06 <51779981+Kavinaya06@users.noreply.github.com>
2023-09-13 16:06:18 +0530

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class forLoop {
+	
+public static void main(String args[])
+{
+int i;
+Scanner sc=new Scanner(System.in);
+for (i=0;i<=10;i++)
+{
+	System.out.println(i);
+}
+}
+}
